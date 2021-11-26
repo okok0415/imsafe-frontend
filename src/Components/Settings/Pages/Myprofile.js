ba3ca234cee88a2c6a1ebc2055a3ebd9f123c577
okok0415 <dlawjdals11@naver.com>
2021-11-26 19:55:38 +0900

    211126 Refactoring

--- src/Components/Settings/Pages/Myprofile.js
+++ src/Components/Settings/Pages/Myprofile.js
@@ -20,6 +20,7 @@ function Myprofile() {
         })
     }, []);
 
+
     return (
         <>
             <div className='myprofile-content'>
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -11,15 +11,6 @@ export const request = async (method, url, data) => {
         .catch((err) => console.log(err));
 };
 
-export const requestMedia = async (method, url, data) => {
-    return await fetch(DOMAIN + url, {
-        method,
-        body: data
-    })
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err));
-};
-
 export const requestGet = async (method, url,) => {
     return await fetch(DOMAIN + url, {
         method,
